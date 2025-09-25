c07031e416a12ffd91e31cc0cf8f0671c3883182
Jok <jokwiyual@outlook.com>
2025-09-24 22:04:45 -0600

    connect front to back and display articles

---
+++ Inventors_Frontend/src/api.js
@@ -0,0 +1,14 @@
+// src/api.js
+const API_URL = "http://127.0.0.1:8000/articles/";
+
+export async function fetchArticles() {
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) throw new Error("Network response was not ok");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+    return [];
+  }
+}
